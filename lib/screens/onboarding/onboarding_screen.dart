044235f3807d4e89f985bb3871690ebf5b73d95b
Panasoft94 <webmasterdjim@gmail.com>
2025-10-29 16:40:40 +0100

    Correction des bugs de la redirecion du fihicer create_pin

--- lib/screens/onboarding/onboarding_screen.dart
+++ lib/screens/onboarding/onboarding_screen.dart
@@ -1,4 +1,4 @@
-import 'package:budget/screens/login.dart';
+import 'package:budget/screens/create_pin.dart';
 import 'package:flutter/material.dart';
 
 class OnboardingScreen extends StatelessWidget {
@@ -39,7 +39,7 @@ class OnboardingScreen extends StatelessWidget {
                 onPressed: () {
                   Navigator.of(context).pushReplacement(
                     MaterialPageRoute(
-                      builder: (BuildContext context) => const LoginPage(),
+                      builder: (BuildContext context) => const CreatePinPage(),
                     ),
                   );
                 },
